fb745f0603517ecde68bd234f28161a2b27ad9a2
larenzzx <larenztabotabo@gmail.com>
2026-03-12 17:16:55 -0400

    skill section center title

--- src/components/skills/Skill-info.jsx
+++ src/components/skills/Skill-info.jsx
@@ -1,7 +1,7 @@
 export const SkillInfo = ({ info }) => {
   return (
     <div className="mb-8">
-      <h3 className="mb-2 text-xl font-bold text-base-content md:text-2xl">
+      <h3 className="mb-2 text-xl font-bold text-base-content md:text-2xl text-center">
         {info}
       </h3>
       <div className="mx-auto h-0.5 w-16 rounded-full bg-accent"></div>
